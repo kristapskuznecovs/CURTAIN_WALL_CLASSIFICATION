93ea7d4ef6c01a23c974d32fdfa4973c8d21ab7a
kristapskuznecovs <kr1s@inbox.lv>
2023-07-24 16:20:42 +0300

    Icon and page container update

--- cw_frontend/src/pages/AboutPage.js
+++ cw_frontend/src/pages/AboutPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div style={{ textAlign: 'left', marginTop: '20px' }}>
-      <p>Par šo projektu jautā Jānim Goldmanim un Kristapam Kuzņecovam.</p>
+      <p>Par šo projektu jautā Jānim Goldmanim un Kristapam Kuzņecovam!</p>
     </div>
   );
 };
