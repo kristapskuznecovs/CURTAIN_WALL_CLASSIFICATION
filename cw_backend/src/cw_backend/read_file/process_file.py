30e0019be6e0e958d7886f96b9d204808a07469c
Janis.Goldmanis <janis.goldmanis@gmail.com>
2023-08-07 11:50:56 +0300

    Added output file deletion

--- cw_backend/src/cw_backend/read_file/process_file.py
+++ cw_backend/src/cw_backend/read_file/process_file.py
@@ -1,10 +1,14 @@
 import os
+import time
 from . import point_cloud, read_profile_csv
 from ..write_file import draw_svg, analyze_element_difference, analyze_jsons
 from ..write_file import write_json
 from ..classes.element_representation import opening
 from .. import settings
 from ..errors import error_handling
+from ..errors import verification
+from ..read_file import sort_files
+from ..classes.other import geometry
 
 results = {"Errors": error_handling.errors}  # Store the results
 
@@ -18,19 +22,34 @@ def set_current_directory(current_directory):
     current_dir = current_directory
 
 
-def process_files(filename):
+def process_files(filenames):
     global current_dir
 
+    output_folder = os.path.join(current_dir, 'data', settings["output_folder"])
+    write_json.delete_files_in_folder(output_folder)
+
+    start = time.time()
+
+    files = sort_files.sort_files(filenames, current_dir)
+
+    print('Files sorted in', time.time() - start)
+
+    start = time.time()
+
+    if len(files["unknown_files"]) > 0:
+        print('Duplicate report files')
+        for file in files["unknown_files"]:
+            print(file)
+        return False
+
     # Input
-    opening_report = os.path.join(current_dir, 'node_input', 'OpeningData.csv')
+    opening_report = files["opening_file"]
 
     # profile_report = os.path.join(current_dir, 'Aile_Element_Classification', 'node_input', filename)
-    profile_report = os.path.join(current_dir, 'data', settings["node_input"], filename)
+    profile_report = files["profile_file"]
 
     # Output
     json_folder = os.path.join(current_dir, 'data', settings["json_folder"])
-
-    output_folder = os.path.join(current_dir, 'data', settings["output_folder"])
 
     svg_folder = os.path.join(current_dir, 'data', settings["svg_folder"])
     write_jsons = settings["write_jsons"]
@@ -42,14 +61,14 @@ def process_files(filename):
     assign_opening_type = settings["assign_opening_type"]
 
     # Group profiles by GUID's into distinct element objects
-    elements = read_profile_csv.read_csv(profile_report)
+    elements, bad_elements = read_profile_csv.read_csv(profile_report)
 
     if not elements:
         return False
 
     if assign_opening_type:
         # Get point cloud from Opening Report
-        point_cloud_array = point_cloud.get_point_cloud_array(opening_report)
+        opening_data_tree = point_cloud.get_physical_opening_data_tree(opening_report)
 
     print('Generating openings, writing')
     print('Working...')
@@ -59,22 +78,37 @@ def process_files(filename):
     write_json.delete_files_in_folder(json_folder)
     write_json.delete_files_in_folder(svg_folder)
 
+    print('elements generated in ', time.time() - start)
+
+    start = time.time()
+
     for element in elements:
         # try:
         for plane in element.element_planes:
             plane.generate_openings()
+
+    for element in elements[:]:
+        if not verification.left_or_right_side_as_single_profile(element):
+            bad_elements.append(element)
+            elements.remove(element)
+
+    print('elements verified and openings generated in', time.time() - start)
+
+    for element in elements:
         for plane in element.element_planes:
             if assign_opening_type:
-                opening.assign_opening_type(plane.opening, plane.plane, point_cloud_array)
+                opening.find_opening_types_for_plane(plane, opening_data_tree)
+
         if draw_element:
             draw_svg.draw_element(element, svg_folder)
         if write_jsons:
             write_json.write_json(element, json_folder)
+
         # except:
         #     print('Problem', element.guid)
     if analyze_json:
         print('Analyzing JSONs')
-        print('Working')
+        print('Working...')
         analyze_jsons.analyze_jsons(output_folder, json_folder)
 
         if settings["analyze_differences"]:
@@ -83,4 +117,11 @@ def process_files(filename):
                                                                                          difference_folder)
 
         # results[filename] = result  # Store the result using the filename as the key
+        print('Finished')
+
+    if len(bad_elements) > 0:
+        print('\nBad Elements:')
+        for element in bad_elements:
+            print(element.guid)
+
     return True
